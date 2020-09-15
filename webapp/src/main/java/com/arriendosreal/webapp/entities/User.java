b51e07f326d5840bcb0770976eef731af09ddcd9
Nunoporing <bahamondesbp@gmail.com>
2020-09-15 01:27:33 -0300

    Connected to Oracle DB, started entity

---
+++ webapp/src/main/java/com/arriendosreal/webapp/entities/User.java
@@ -0,0 +1,40 @@
+package com.arriendosreal.webapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	private String email;
+	private String profile;
+	private String password;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getProfile() {
+		return profile;
+	}
+	
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
